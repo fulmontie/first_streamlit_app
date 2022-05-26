9cfd4956e4a2ef042ab2e8a97e94267ab915abd7
Venkat Annamraju <106280281+fulmontie@users.noreply.github.com>
2022-05-26 15:36:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text ('Idly with peanut chutney')
 streamlit.text ('Vada with Sambar')
 streamlit.text ('Upma and chutney')
+import.pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
