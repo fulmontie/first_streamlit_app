6021f6c262e84b53a99c618377791318afd66f62
Venkat Annamraju <106280281+fulmontie@users.noreply.github.com>
2022-05-26 16:22:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text ('Idly with peanut chutney')
 streamlit.text ('Vada with Sambar')
 streamlit.text ('Upma and chutney')
-streamlit.header ('Build your own Fruit Smoothie')
+streamlit.header ('🍌🥭 Build your own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
