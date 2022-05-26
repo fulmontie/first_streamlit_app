39bb7b3f1054eea73a07b8e3f469e6466f9d1b44
Venkat Annamraju <106280281+fulmontie@users.noreply.github.com>
2022-05-26 15:24:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text ('Idly with peanut chutney')
+streamlit.text ('Vada with Sambar')
+streamlit.text ('Upma and chutney')
